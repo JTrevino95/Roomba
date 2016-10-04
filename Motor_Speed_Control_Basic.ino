e9c2bd4ed40c728b1993b606a6e88edbc9e1fd57
JTrevino95 <jb.trevino95@gmail.com>
2016-10-03 23:34:38 -0400

    Add files via upload

---
+++ Motor_Speed_Control_Basic.ino
@@ -0,0 +1,18 @@
+/* This alternates the speed on a DC motor */
+
+int base = 3;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  pinMode(base, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(base, HIGH);   // motor max
+  delay(5000);              // wait for 5 seconds
+  analogWrite(base, 125);    // motor roughly in the middle range
+  delay(5000);              // wait for 5 seconds
+  
+}
