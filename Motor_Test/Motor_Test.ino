56111b7b085b585f6e56bc36379b6ce6eed14054
JTrevino95 <dillonmoravek@gmail.com>
2016-10-25 12:33:32 -0400

    Adding Joe's files from the TALON-PC

---
+++ Motor_Test/Motor_Test.ino
@@ -0,0 +1,25 @@
+#include <AFMotor.h>
+AF_DCMotor motorLeft(3, MOTOR12_64KHZ);
+AF_DCMotor motorRight(4, MOTOR12_64KHZ);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+Serial.begin(9600);
+
+motorRight.setSpeed(255);
+motorLeft.setSpeed(255);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+motorLeft.run(FORWARD);
+motorRight.run(FORWARD);
+delay(2000);
+motorLeft.run(FORWARD);
+motorRight.run(BACKWARD);
+delay(1000);
+
+}
