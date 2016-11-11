f27dfc450feb7e8d0d6508fb61cf4647c14e1898
talon <talon>
2016-11-11 14:32:24 -0500

    Adding Joe's changes

--- Encoder_Testing/Encoder_Testing.ino
+++ Encoder_Testing/Encoder_Testing.ino
@@ -2,8 +2,8 @@
 /* This is a quick program to monitor the status of the encoders
 to measure the 'ticks' per revolution */
 
-int encoderLeft = A0; // left encoder is on anaog pin A0, which will function as a digital pin
-int encoderRight = A1; // right encoder is on analog pin A1, which will function as a digital pin
+int encoderLeft = 2; // left encoder is on anaog pin A0, which will function as a digital pin
+int encoderRight = 3; // right encoder is on analog pin A1, which will function as a digital pin
 int count = 0; // start the count at 0
 int encoderLeftLast = 0;
 int encoderRightLast = 0;
@@ -13,14 +13,14 @@ void setup()
 pinMode(encoderLeft,INPUT);
 pinMode(encoderRight,INPUT);
 Serial.begin(9600);
-//attachInterrupt(digitalPinToInterrupt(encoderLeft),counter,RISING); 
+attachInterrupt(digitalPinToInterrupt(encoderLeft),counter,RISING); // count the amount of ticks in one revolution
 }
 
 void loop() 
 {
-      // put your main code here, to run repeatedly:
-    // read the state of the encoder pins
+     // put your main code here, to run repeatedly:
     
+    // read the state of the encoder pins
     int encoderLeftStatus = digitalRead(encoderLeft);
     int encoderRightStatus = digitalRead(encoderRight);
     
@@ -28,28 +28,18 @@ void loop()
     //Serial.print("Left: ");Serial.print(encoderLeftStatus); 
     //Serial.print("\t Right: ");Serial.println(encoderRightStatus);
     
-    /*Counting the amount of 1s that pass by on encoderLeft
-    so we can count the amount of ticks in one revolution,
-    it should be equal for both encoders, but the if statement 
-    can be changed to count for encoderRight*/
-    if (encoderRightStatus == 1 && encoderRightLast == 0)
-      {
-        counter();
-      }
-     
-    //print the amount of ticks
-    Serial.print("Counter: "); Serial.println(count);
-    encoderRightLast = encoderRightStatus;
 }
 
 //increments a counter
 void counter()
 {
   count = count + 1;
+  Serial.println(count);
 }
 
 
 // distance traveled = (circumfrence)*(#revolutions)
+//revolutions = (distance)/(circumfrence)
 // #ticks = (ticks/revolution) * (#revolutions)
 // wheel diameter is 2.3 - 2.5 inches
 // circumfrence is roughly (2.4)pi = 7.536 inches
